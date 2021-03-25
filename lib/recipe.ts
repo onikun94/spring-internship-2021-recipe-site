9e92b7fcee35ef175a2025520434806d75c2f03a
onikun94 <onikun94@gmail.com>
2021-03-25 14:51:52 +0900

    add search and page

--- lib/recipe.ts
+++ lib/recipe.ts
@@ -17,15 +17,62 @@ export type Recipe = {
   related_recipes: number[];
 };
 
-export async function getRecipes(): Promise<Recipe[]> {
+export type Links = {
+  next?: string;
+  prev?: string;
+};
+
+export type Res = {
+  recipes: Recipe[];
+  links: Links;
+};
+
+export async function getRecipes(): Promise<Res> {
   const response = await fetch(
     "https://internship-recipe-api.ckpd.co/recipes",
     {
-      headers: { "X-Api-Key": "remark-fish-magician" },
+      headers: { "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY as string },
+      method: "GET",
     }
   );
-  const jsonData = await response.json();
-  const links = jsonData?.links;
-  const recipes = jsonData?.recipes;
-  return recipes as Recipe[];
+  const recipes = await response.json();
+  //const links = jsonData?.links;
+  //const recipes = jsonData?.recipes;
+  return recipes as Res;
+}
+
+export async function getPages(nextLink: string): Promise<Res> {
+  const response = await fetch(nextLink, {
+    headers: { "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY as string },
+    method: "GET",
+  });
+  const recipes = await response.json();
+  //const links = jsonData?.links;
+  //const recipes = jsonData?.recipes;
+  return recipes as Res;
+}
+
+export async function getRecipe(id: number): Promise<Res | null> {
+  const response = await fetch(
+    `https://internship-recipe-api.ckpd.co/recipes/${id}`,
+    {
+      headers: { "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY as string },
+      method: "GET",
+    }
+  );
+  const recipe = await response.json();
+  return recipe as Res;
+}
+
+export async function search(keyword: string): Promise<Res> {
+  const response = await fetch(
+    `https://internship-recipe-api.ckpd.co/search?keyword=${keyword}`,
+    {
+      headers: { "X-Api-Key": process.env.NEXT_PUBLIC_API_KEY as string },
+      method: "GET",
+    }
+  );
+
+  const searchRes = await response.json();
+  return searchRes as Res;
 }
